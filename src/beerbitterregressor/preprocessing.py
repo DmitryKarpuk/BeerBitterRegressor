0fc015a55af3c8ec750afc093293f88a010025dd
DmitryKarpuk <dimakarpuk22@gmail.com>
2022-11-01 20:48:15 +0300

    Fix predict script

--- src/beerbitterregressor/preprocessing.py
+++ src/beerbitterregressor/preprocessing.py
@@ -4,30 +4,30 @@ import numpy as np
 
 def clean_data(df):
     # Drop text features
-    df.drop(columns=['description', 'name'], inplace=True)
+    df.drop(columns=["description", "name"], inplace=True)
     # prepare srm feature
-    srm_over_40_idx = (pd.to_numeric(df[df.srm != 'Over 40'].srm) > 40).values
-    num_srm = pd.to_numeric(df[df.srm != 'Over 40'].srm)
+    srm_over_40_idx = (pd.to_numeric(df[df.srm != "Over 40"].srm) > 40).values
+    num_srm = pd.to_numeric(df[df.srm != "Over 40"].srm)
     srm_over_40_mean = int(num_srm[num_srm > 40].mean())
     srm_over_40_mean
-    df.replace({'srm' : {'Over 40' : str(srm_over_40_mean)}}, inplace=True)
-    df = df.astype({'srm' : 'int32'})
+    df.replace({"srm": {"Over 40": str(srm_over_40_mean)}}, inplace=True)
+    df = df.astype({"srm": "int32"})
     # replace nan values of feature glass
-    df.glass = df.glass.fillna('Nglass')
+    df.glass = df.glass.fillna("Nglass")
     # convert isOrganic column into numeric
-    df.isOrganic = (df.isOrganic == 'Y').astype('int32')
+    df.isOrganic = (df.isOrganic == "Y").astype("int32")
     # clip originalGravity
     df.originalGravity = df.originalGravity.clip(upper=1.13)
     # limit ibu if train preparation
-    if 'ibu' in df.columns:
+    if "ibu" in df.columns:
         df.ibu = df.ibu.clip(upper=120)
     return df
 
 
 def add_new_features(df):
-    df['abv_mul_grav'] = df.abv * df.originalGravity
-    df['abv_mul_srm'] = df.abv * df.srm
-    df['srm_div_abv'] = df.srm / df.abv
-    df['srm_mull_grav'] = df.srm * df.originalGravity
-    df['srm_mull_grav_div_abv'] = df.srm * df.originalGravity / df.abv
+    df["abv_mul_grav"] = df.abv * df.originalGravity
+    df["abv_mul_srm"] = df.abv * df.srm
+    df["srm_div_abv"] = df.srm / df.abv
+    df["srm_mull_grav"] = df.srm * df.originalGravity
+    df["srm_mull_grav_div_abv"] = df.srm * df.originalGravity / df.abv
     return df
--- src/beerbitterregressor/train.py
+++ src/beerbitterregressor/train.py
@@ -6,58 +6,58 @@ from catboost import CatBoostRegressor, Pool
 from .preprocessing import clean_data, add_new_features
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 SEED = 42
-CAT_FEATURES = ['available', 'glass']
+CAT_FEATURES = ["available", "glass"]
+
 
 @click.command()
 @click.option(
-    '-d',
-    '--dataset-path',
-    default='data/beer_train.csv',
-    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    "-d",
+    "--dataset-path",
+    default="data/beer_train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 @click.option(
-    '-m',
-    '--model-path',
-    default='models/model.cbn',
-    type=click.Path(writable=True, dir_okay=False, path_type=Path)
+    "-m",
+    "--model-path",
+    default="models/model.cbn",
+    type=click.Path(writable=True, dir_okay=False, path_type=Path),
 )
 @click.option(
-    '-p',
-    '--param-path',
-    default='config/model_params.pkl',
-    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    "-p",
+    "--param-path",
+    default="config/model_params.pkl",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 def train(
     dataset_path: Path,
     model_path: Path,
     param_path: Path,
-)-> None:
-    '''
+) -> None:
+    """
     Trains a catboost model on beer dataset with ertain parameters.
-    '''
+    """
 
-# Make model and load parameters
-    model = CatBoostRegressor(verbose=1000, loss_function='RMSE')
-    with open(param_path, 'rb') as f:
+    # Make model and load parameters
+    model = CatBoostRegressor(verbose=1000, loss_function="RMSE")
+    with open(param_path, "rb") as f:
         model_params = load(f)
     model.set_params(**model_params)
 
-# Load and prepare data
-    data = pd.read_csv(dataset_path)
+    # Load and prepare data
+    data = pd.read_csv(dataset_path, index_col=['id'])
     df = clean_data(data)
     df = add_new_features(df).reset_index(drop=True)
-    train_features = [x for x in df.columns if x != 'ibu']
-    y_train = df['ibu'].values
-    X_train = df[train_features].values
-    cat_features_idx = [train_features.index(i)
-                        for i in CAT_FEATURES
-                        ]
+    train_features = [x for x in df.columns if x != "ibu"]
+    y_train = df["ibu"].values
+    X_train = df[train_features].reset_index(drop=True)
+    cat_features_idx = [train_features.index(i) for i in CAT_FEATURES]
     train_pool = Pool(X_train, y_train, cat_features=cat_features_idx)
 
-# Train and save model
+    # Train and save model
     model.fit(train_pool)
     model.save_model(model_path)
     click.echo(click.style("Model was successful saved.", fg="green"))
